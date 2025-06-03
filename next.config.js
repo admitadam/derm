47516e4328276cb22910c06bd03b29a4933b8f55
admitadam <adam@admit.org>
2025-06-02 23:39:42 -0400

    Update frontend to connect directly to backend API

--- next.config.js
+++ next.config.js
@@ -6,26 +6,6 @@ const nextConfig = {
   },
   // Remove basePath when using custom domain
   // basePath: '/derm',
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://localhost:5000/:path*',
-      },
-    ]
-  },
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET, POST, PUT, DELETE, OPTIONS' },
-          { key: 'Access-Control-Allow-Headers', value: 'Content-Type, Authorization' },
-        ],
-      },
-    ]
-  },
 }
 
 module.exports = nextConfig 
